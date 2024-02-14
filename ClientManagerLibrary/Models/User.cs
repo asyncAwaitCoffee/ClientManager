291a5e70dcebd2ce7b0730982aaca7e8d7da3250
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-14 23:29:03 +0400

    console test select

--- ClientManagerLibrary/Models/User.cs
+++ ClientManagerLibrary/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace ClientManagerLibrary.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Password { get; set; }
     }
 }
