d97dfbb3faaa8239a41a921842d9d1164fce19a3
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-16 15:32:11 +0400

    notify icon text

--- ClientManagerForms/ClientsForm.Designer.cs
+++ ClientManagerForms/ClientsForm.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            DataGridViewCellStyle dataGridViewCellStyle13 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle14 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle11 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle12 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientsForm));
             clientsDataGridView = new DataGridView();
             clientId = new DataGridViewTextBoxColumn();
@@ -53,27 +53,27 @@
             clientsDataGridView.AllowUserToDeleteRows = false;
             clientsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             clientsDataGridView.Columns.AddRange(new DataGridViewColumn[] { clientId, surName, fullName, accountId, checkClientColumn });
-            dataGridViewCellStyle13.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle13.BackColor = SystemColors.Window;
-            dataGridViewCellStyle13.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            dataGridViewCellStyle13.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle13.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle13.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle13.WrapMode = DataGridViewTriState.False;
-            clientsDataGridView.DefaultCellStyle = dataGridViewCellStyle13;
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = SystemColors.Window;
+            dataGridViewCellStyle6.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle6.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.False;
+            clientsDataGridView.DefaultCellStyle = dataGridViewCellStyle6;
             clientsDataGridView.Location = new Point(10, 115);
             clientsDataGridView.Margin = new Padding(2);
             clientsDataGridView.Name = "clientsDataGridView";
             clientsDataGridView.ReadOnly = true;
             clientsDataGridView.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Sunken;
-            dataGridViewCellStyle14.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle14.BackColor = Color.Silver;
-            dataGridViewCellStyle14.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            dataGridViewCellStyle14.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle14.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle14.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle14.WrapMode = DataGridViewTriState.True;
-            clientsDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle14;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = Color.Silver;
+            dataGridViewCellStyle7.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle7.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            clientsDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle7;
             clientsDataGridView.RowTemplate.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             clientsDataGridView.RowTemplate.DefaultCellStyle.Padding = new Padding(5);
             clientsDataGridView.RowTemplate.Height = 200;
@@ -84,8 +84,8 @@
             // clientId
             // 
             clientId.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            clientId.DefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            clientId.DefaultCellStyle = dataGridViewCellStyle1;
             clientId.FillWeight = 20F;
             clientId.HeaderText = "Id";
             clientId.Name = "clientId";
@@ -95,8 +95,8 @@
             // surName
             // 
             surName.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            surName.DefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            surName.DefaultCellStyle = dataGridViewCellStyle2;
             surName.FillWeight = 40F;
             surName.HeaderText = "SurName";
             surName.Name = "surName";
@@ -106,8 +106,8 @@
             // fullName
             // 
             fullName.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dataGridViewCellStyle10.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            fullName.DefaultCellStyle = dataGridViewCellStyle10;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            fullName.DefaultCellStyle = dataGridViewCellStyle3;
             fullName.FillWeight = 40F;
             fullName.HeaderText = "Full Name";
             fullName.Name = "fullName";
@@ -117,8 +117,8 @@
             // accountId
             // 
             accountId.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dataGridViewCellStyle11.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            accountId.DefaultCellStyle = dataGridViewCellStyle11;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            accountId.DefaultCellStyle = dataGridViewCellStyle4;
             accountId.FillWeight = 30F;
             accountId.HeaderText = "AccountId";
             accountId.Name = "accountId";
@@ -128,10 +128,10 @@
             // checkClientColumn
             // 
             checkClientColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dataGridViewCellStyle12.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle12.NullValue = false;
-            dataGridViewCellStyle12.Padding = new Padding(5);
-            checkClientColumn.DefaultCellStyle = dataGridViewCellStyle12;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.NullValue = false;
+            dataGridViewCellStyle5.Padding = new Padding(5);
+            checkClientColumn.DefaultCellStyle = dataGridViewCellStyle5;
             checkClientColumn.FillWeight = 20F;
             checkClientColumn.HeaderText = "VIP";
             checkClientColumn.Name = "checkClientColumn";
@@ -141,10 +141,10 @@
             // clientsFormNotifyIcon
             // 
             clientsFormNotifyIcon.BalloonTipIcon = ToolTipIcon.Info;
-            clientsFormNotifyIcon.BalloonTipText = "Чтобы раскрыть приложение, нажмите на иконку";
-            clientsFormNotifyIcon.BalloonTipTitle = "Подсказка";
+            clientsFormNotifyIcon.BalloonTipText = "Click to expand window";
+            clientsFormNotifyIcon.BalloonTipTitle = "Clients Manager";
             clientsFormNotifyIcon.Icon = (Icon)resources.GetObject("clientsFormNotifyIcon.Icon");
-            clientsFormNotifyIcon.Text = "notifyIcon1";
+            clientsFormNotifyIcon.Text = "Clients Manager";
             clientsFormNotifyIcon.Visible = true;
             clientsFormNotifyIcon.BalloonTipClicked += clientsFormNotifyIcon_BalloonTipClicked;
             clientsFormNotifyIcon.MouseClick += clientsFormNotifyIcon_MouseClick;
